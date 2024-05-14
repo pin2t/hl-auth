ab079dcc50a016701389bbb0e768e13b1dce42ad
pin2t <pin2t@mail.ru>
2024-05-14 07:56:30 +0300

    fix

--- src/main/java/SimpleTasks.java
+++ src/main/java/SimpleTasks.java
@@ -29,36 +29,63 @@ public class SimpleTasks {
     void run() {
         try {
             final long started = System.nanoTime();
-            var connection = new Socket(InetAddress.getByName("localhost"), 8080);
+            var tasks = new ConcurrentLinkedQueue<Task>();
             var input = new BufferedReader(new FileReader("data/tasks.jsonl"));
-            try {
-                var lines = input.lines().toList();
-                for (var l : lines) {
-                    if (connection.isClosed()) {
-                        connection = new Socket(InetAddress.getByName("localhost"), 8080);
-                    }
+            input.lines().forEach(l -> {
+                try {
+                    tasks.offer(new Task(l, done));
+                } catch (ParseException e) {
+                    err.println("error parsing " + l + ": " + e.getMessage());
+                }
+            });
+            var total = tasks.size();
+            var threads = Executors.newVirtualThreadPerTaskExecutor();
+            for (int i = 0; i < (sequential ? 1 : 200); i++) {
+                threads.submit(() -> {
                     try {
-                        var t = new Task(l, done);
+                        Socket connection = new Socket(InetAddress.getByName("localhost"), 8080);
                         try {
-                            if (!t.run(connection)) {
-                                errors.addAndGet(1);
+                            Task t;
+                            while ((t = tasks.poll()) != null) {
+                                if (connection.isClosed()) {
+                                    connection = new Socket(InetAddress.getByName("localhost"), 8080);
+                                }
+                                try {
+                                    while (!t.ready()) {
+                                        Thread.sleep(50);
+                                    }
+                                    if (!t.run(connection)) {
+                                        errors.addAndGet(1);
+                                        if (stopOnError) {
+                                            break;
+                                        }
+                                    }
+                                } catch (SocketException e) {
+                                    err.println("socket error " + e.getMessage());
+                                    connection.close();
+                                } catch (InterruptedException e) {
+                                    throw new RuntimeException(e);
+                                } finally {
+                                    done.add(t.id);
+                                }
                             }
                         } finally {
-                            done.add(t.id);
+                            try {
+                                connection.close();
+                            } catch (IOException e) {
+                                err.println("io error " + e.getMessage());
+                            }
                         }
-                    } catch (ParseException e) {
-                        err.println("parse error " + e.getMessage() + " text \"" + l + "\"");
-                    } catch (SocketException e) {
-                        err.println("socket error " + e.getMessage());
-                        connection.close();
+                    } catch (IOException e) {
+                        err.println("io error " + e.getMessage());
                     }
-                    out.printf("\r%d/%d\t%d rps %d errors\t\t\t\r", done.size(), lines.size(),
-                        done.size() / max((System.nanoTime() - started) / 1000000000, 1), errors.get()
-                    );
-                }
-            } finally {
-                connection.close();
-                input.close();
+                });
+            }
+            while (done.size() < total && (!stopOnError || errors.get() == 0)) {
+                out.printf("\r%d/%d\t%d rps %d errors\t\t\t\r", done.size(), total,
+                    done.size() / max((System.nanoTime() - started) / 1000000000, 1), errors.get()
+                );
+                Thread.sleep(200);
             }
         } catch (Exception e) {
             err.println("unhandled exception" + e.getMessage());
@@ -122,7 +149,7 @@ public class SimpleTasks {
                 rq.print(request);
                 headers.forEach((k, v) -> rq.print(k + ": " + v + "\r\n"));
                 rq.print("Host: localhost\r\n");
-                rq.print("Content-Length: " + body.length() + "\r\n");
+                rq.print("Content-Length: " + body.getBytes(StandardCharsets.UTF_8).length + "\r\n");
                 rq.print("Connection: keep-alive\r\n");
                 if (!body.isBlank()) {
                     rq.print("Content-Type: application/json\r\n");
@@ -146,14 +173,14 @@ public class SimpleTasks {
                 }
             } while (line != null && !line.isBlank());
             var len = Integer.parseInt(rsHeaders.getOrDefault("Content-Length", "0"));
+            var rsBody = new StringBuilder();
             if (len > 0) {
-                var body = new StringBuilder();
                 var readLen = 0;
                 char[] buf = new char[512];
                 do {
                     int n = rs.read(buf);
                     if (n > 0) {
-                        body.append(buf, 0, n);
+                        rsBody.append(buf, 0, n);
                         readLen += new String(buf, 0, n).getBytes(StandardCharsets.UTF_8).length;
                     }
                 } while (readLen < len);
